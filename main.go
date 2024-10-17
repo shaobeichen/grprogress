e6a82f17a04401d4218ddf6439bd62c14bfa3ae1
shaobeichen <shaobeichen@outlook.com>
2024-10-17 11:46:00 +0800

    feat: init template

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-// An example demonstrating an application with multiple views.
-//
-// Note that this example was produced before the Bubbles progress component
-// was available (github.com/charmbracelet/bubbles/progress) and thus, we're
-// implementing a progress bar from scratch here.
-
 import (
 	"fmt"
 	"math"
@@ -28,20 +22,23 @@ const (
 
 // General stuff for styling the view
 var (
-	keywordStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("211"))
 	subtleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
-	ticksStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("79"))
-	checkboxStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212"))
 	progressEmpty = subtleStyle.Render(progressEmptyChar)
-	dotStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("236")).Render(dotChar)
 	mainStyle     = lipgloss.NewStyle().MarginLeft(2)
 
 	// Gradient colors we'll use for the progress bar
 	ramp = makeRampStyles("#B14FFF", "#00FFA3", progressBarWidth)
 )
 
+type model struct {
+	Chosen   bool
+	Frames   int
+	Progress float64
+	Loaded   bool
+}
+
 func main() {
-	initialModel := model{0, false, 10, 0, 0, false, false}
+	initialModel := model{false, 0, 0, false}
 	p := tea.NewProgram(initialModel)
 	if _, err := p.Run(); err != nil {
 		fmt.Println("could not start program:", err)
@@ -49,15 +46,8 @@ func main() {
 }
 
 type (
-	tickMsg  struct{}
 	frameMsg struct{}
 )
-
-func tick() tea.Cmd {
-	return tea.Tick(time.Second, func(time.Time) tea.Msg {
-		return tickMsg{}
-	})
-}
 
 func frame() tea.Cmd {
 	return tea.Tick(time.Second/60, func(time.Time) tea.Msg {
@@ -65,18 +55,8 @@ func frame() tea.Cmd {
 	})
 }
 
-type model struct {
-	Choice   int
-	Chosen   bool
-	Ticks    int
-	Frames   int
-	Progress float64
-	Loaded   bool
-	Quitting bool
-}
-
 func (m model) Init() tea.Cmd {
-	return tick()
+	return frame()
 }
 
 // Main update function.
@@ -85,65 +65,16 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	if msg, ok := msg.(tea.KeyMsg); ok {
 		k := msg.String()
 		if k == "q" || k == "esc" || k == "ctrl+c" {
-			m.Quitting = true
 			return m, tea.Quit
 		}
-	}
-
-	// Hand off the message and model to the appropriate update function for the
-	// appropriate view based on the current state.
-	if !m.Chosen {
-		return updateChoices(msg, m)
 	}
 	return updateChosen(msg, m)
 }
 
 // The main view, which just calls the appropriate sub-view
 func (m model) View() string {
-	var s string
-	if m.Quitting {
-		return "\n  See you later!\n\n"
-	}
-	if !m.Chosen {
-		s = choicesView(m)
-	} else {
-		s = chosenView(m)
-	}
+	s := chosenView(m)
 	return mainStyle.Render("\n" + s + "\n\n")
-}
-
-// Sub-update functions
-
-// Update loop for the first view where you're choosing a task.
-func updateChoices(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "j", "down":
-			m.Choice++
-			if m.Choice > 3 {
-				m.Choice = 3
-			}
-		case "k", "up":
-			m.Choice--
-			if m.Choice < 0 {
-				m.Choice = 0
-			}
-		case "enter":
-			m.Chosen = true
-			return m, frame()
-		}
-
-	case tickMsg:
-		if m.Ticks == 0 {
-			m.Quitting = true
-			return m, tea.Quit
-		}
-		m.Ticks--
-		return m, tick()
-	}
-
-	return m, nil
 }
 
 // Update loop for the second view after a choice has been made
@@ -156,78 +87,17 @@ func updateChosen(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 			if m.Progress >= 1 {
 				m.Progress = 1
 				m.Loaded = true
-				m.Ticks = 3
-				return m, tick()
+				return m, nil
 			}
 			return m, frame()
 		}
-
-	case tickMsg:
-		if m.Loaded {
-			if m.Ticks == 0 {
-				m.Quitting = true
-				return m, tea.Quit
-			}
-			m.Ticks--
-			return m, tick()
-		}
 	}
-
 	return m, nil
-}
-
-// Sub-views
-
-// The first view, where you're choosing a task
-func choicesView(m model) string {
-	c := m.Choice
-
-	tpl := "What to do today?\n\n"
-	tpl += "%s\n\n"
-	tpl += "Program quits in %s seconds\n\n"
-	tpl += subtleStyle.Render("j/k, up/down: select") + dotStyle +
-		subtleStyle.Render("enter: choose") + dotStyle +
-		subtleStyle.Render("q, esc: quit")
-
-	choices := fmt.Sprintf(
-		"%s\n%s\n%s\n%s",
-		checkbox("Plant carrots", c == 0),
-		checkbox("Go to the market", c == 1),
-		checkbox("Read something", c == 2),
-		checkbox("See friends", c == 3),
-	)
-
-	return fmt.Sprintf(tpl, choices, ticksStyle.Render(strconv.Itoa(m.Ticks)))
 }
 
 // The second view, after a task has been chosen
 func chosenView(m model) string {
-	var msg string
-
-	switch m.Choice {
-	case 0:
-		msg = fmt.Sprintf("Carrot planting?\n\nCool, we'll need %s and %s...", keywordStyle.Render("libgarden"), keywordStyle.Render("vegeutils"))
-	case 1:
-		msg = fmt.Sprintf("A trip to the market?\n\nOkay, then we should install %s and %s...", keywordStyle.Render("marketkit"), keywordStyle.Render("libshopping"))
-	case 2:
-		msg = fmt.Sprintf("Reading time?\n\nOkay, cool, then we’ll need a library. Yes, an %s.", keywordStyle.Render("actual library"))
-	default:
-		msg = fmt.Sprintf("It’s always good to see friends.\n\nFetching %s and %s...", keywordStyle.Render("social-skills"), keywordStyle.Render("conversationutils"))
-	}
-
-	label := "Downloading..."
-	if m.Loaded {
-		label = fmt.Sprintf("Downloaded. Exiting in %s seconds...", ticksStyle.Render(strconv.Itoa(m.Ticks)))
-	}
-
-	return msg + "\n\n" + label + "\n" + progressbar(m.Progress) + "%"
-}
-
-func checkbox(label string, checked bool) string {
-	if checked {
-		return checkboxStyle.Render("[x] " + label)
-	}
-	return fmt.Sprintf("[ ] %s", label)
+	return progressbar(m.Progress) + "%"
 }
 
 func progressbar(percent float64) string {
