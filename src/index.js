5a2ff65b5920413160f95e6da16464e9b0bdb437
shaobeichen <shaobeichen@outlook.com>
2024-10-17 16:28:29 +0800

    feat: update template

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 export function update(current) {
   // 获取当前工作目录并构建完整路径
-  const command = path.join(process.cwd(), './bin/hello_windows_amd64.exe')
+  const command = path.join(process.cwd(), './bin/grprogress_windows_amd64.exe')
   const args = [String(current)]
 
   const result = spawnSync(command, args, {
