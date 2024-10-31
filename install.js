962a5a3857419ff2ab33be869bddfa098a6972c6
shaobeichen <shaobeichen@outlook.com>
2024-10-31 16:43:16 +0800

    feat: update install file logic

--- install.js
+++ install.js
@@ -1,101 +1,121 @@
 // https://www.npmjs.com/package/esbuild?activeTab=code
 
+import fs from 'fs'
+import os from 'os'
+import path from 'path'
+import zlib from 'zlib'
+import child_process from 'child_process'
 import https from 'https'
+import { version as versionFromPackageJSON } from './package.json'
+import { fileURLToPath } from 'url'
 
-function installUsingNPM(pkg, subpath, binPath) {
-  const env = { ...process.env, npm_config_global: void 0 }
-  const esbuildLibDir = path2.dirname(require.resolve('esbuild'))
-  const installDir = path2.join(esbuildLibDir, 'npm-install')
-  fs2.mkdirSync(installDir)
-  try {
-    fs2.writeFileSync(path2.join(installDir, 'package.json'), '{}')
-    child_process.execSync(
-      `npm install --loglevel=error --prefer-offline --no-audit --progress=false ${pkg}@${versionFromPackageJSON}`,
-      { cwd: installDir, stdio: 'pipe', env },
-    )
-    const installedBinPath = path2.join(installDir, 'node_modules', pkg, subpath)
-    fs2.renameSync(installedBinPath, binPath)
-  } finally {
-    try {
-      removeRecursive(installDir)
-    } catch {}
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const packages = {
+  'darwin arm64': '@grprogress/darwin-arm64',
+  'darwin x64': '@grprogress/darwin-amd64',
+  'linux x64': '@grprogress/linux-amd64',
+  'win32 x64': '@grprogress/windows-amd64',
+}
+
+function getPackageInfoByCurrentPlatform() {
+  const key = `${process.platform} ${os.arch()}`
+  return {
+    packageName: packages[key],
+    platform: process.platform,
   }
 }
 
-function removeRecursive(dir) {
-  for (const entry of fs2.readdirSync(dir)) {
-    const entryPath = path2.join(dir, entry)
-    let stats
-    try {
-      stats = fs2.lstatSync(entryPath)
-    } catch {
-      continue
-    }
-    if (stats.isDirectory()) removeRecursive(entryPath)
-    else fs2.unlinkSync(entryPath)
+function fetch(url) {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (res) => {
+        if ((res.statusCode === 301 || res.statusCode === 302) && res.headers.location)
+          return fetch(res.headers.location).then(resolve, reject)
+        if (res.statusCode !== 200)
+          return reject(new Error(`Server responded with ${res.statusCode}`))
+        let chunks = []
+        res.on('data', (chunk) => chunks.push(chunk))
+        res.on('end', () => resolve(Buffer.concat(chunks)))
+      })
+      .on('error', reject)
+  })
+}
+
+function extractFileFromTarGzip(buffer, subpath) {
+  try {
+    buffer = zlib.unzipSync(buffer)
+  } catch (err) {
+    throw new Error(`Invalid gzip data in archive: ${(err && err.message) || err}`)
   }
-  fs2.rmdirSync(dir)
+  let str = (i, n) => String.fromCharCode(...buffer.subarray(i, i + n)).replace(/\0.*$/, '')
+  let offset = 0
+  subpath = `package/${subpath}`
+  while (offset < buffer.length) {
+    let name = str(offset, 100)
+    let size = parseInt(str(offset + 124, 12), 8)
+    offset += 512
+    if (!isNaN(size)) {
+      if (name === subpath) return buffer.subarray(offset, offset + size)
+      offset += (size + 511) & ~511
+    }
+  }
+  throw new Error(`Could not find ${JSON.stringify(subpath)} in archive`)
 }
 
 async function downloadDirectlyFromNPM(pkg, subpath, binPath) {
+  // https://registry.npmjs.com/@shaoo/cssoo/-/cssoo-2.2.0.tgz
   const url = `https://registry.npmjs.org/${pkg}/-/${pkg.replace(
     '@esbuild/',
     '',
   )}-${versionFromPackageJSON}.tgz`
-  console.error(`[esbuild] Trying to download ${JSON.stringify(url)}`)
   try {
-    fs2.writeFileSync(binPath, extractFileFromTarGzip(await fetch(url), subpath))
-    fs2.chmodSync(binPath, 493)
+    fs.writeFileSync(binPath, extractFileFromTarGzip(await fetch(url), subpath))
+    fs.chmodSync(binPath, 0o755)
   } catch (e) {
-    console.error(`[esbuild] Failed to download ${JSON.stringify(url)}: ${(e && e.message) || e}`)
     throw e
   }
 }
 
-async function checkAndPreparePackage() {
-  if (isValidBinaryPath(ESBUILD_BINARY_PATH)) {
-    if (!fs2.existsSync(ESBUILD_BINARY_PATH)) {
-      console.warn(
-        `[esbuild] Ignoring bad configuration: ESBUILD_BINARY_PATH=${ESBUILD_BINARY_PATH}`,
-      )
-    } else {
-      applyManualBinaryPathOverride(ESBUILD_BINARY_PATH)
-      return
-    }
-  }
-  const { pkg, subpath } = pkgAndSubpathForCurrentPlatform()
-  let binPath
-  try {
-    binPath = require.resolve(`${pkg}/${subpath}`)
-  } catch (e) {
-    console.error(`[esbuild] Failed to find package "${pkg}" on the file system
-  
-  This can happen if you use the "--no-optional" flag. The "optionalDependencies"
-  package.json feature is used by esbuild to install the correct binary executable
-  for your current platform. This install script will now attempt to work around
-  this. If that fails, you need to remove the "--no-optional" flag to use esbuild.
-  `)
-    binPath = downloadedBinPath(pkg, subpath)
-    try {
-      console.error(`[esbuild] Trying to install package "${pkg}" using npm`)
-      installUsingNPM(pkg, subpath, binPath)
-    } catch (e2) {
-      console.error(
-        `[esbuild] Failed to install package "${pkg}" using npm: ${(e2 && e2.message) || e2}`,
-      )
-      try {
-        await downloadDirectlyFromNPM(pkg, subpath, binPath)
-      } catch (e3) {
-        throw new Error(`Failed to install package "${pkg}"`)
-      }
-    }
-  }
-  maybeOptimizePackage(binPath)
+function removeRecursive(dir) {
+  child_process.execSync(`rm -rf ${dir}`, {
+    cwd: installDir,
+    stdio: 'inherit',
+  })
 }
-checkAndPreparePackage().then(() => {
-  if (isToPathJS) {
-    validateBinaryVersion(process.execPath, toPath)
-  } else {
-    validateBinaryVersion(toPath)
+
+function installUsingNPM(pkg, binFilename, binPath) {
+  const tempDir = 'grprogress-npm-install'
+  fs.mkdirSync(tempDir)
+  const installDir = path.resolve(__dirname, tempDir)
+  try {
+    fs.writeFileSync(path.join(installDir, 'package.json'), '{}')
+    child_process.execSync(`npm install ${pkg}@${versionFromPackageJSON} --save`, {
+      cwd: installDir,
+      stdio: 'inherit',
+    })
+    const installedBinPath = path.join(installDir, 'node_modules', pkg, binFilename)
+    fs.renameSync(installedBinPath, binPath)
+  } catch (e) {
+    console.error('[grprogress] Failed to download and install from npm')
+    removeRecursive(installDir)
+    throw e
   }
-})
+}
+
+async function main() {
+  try {
+    const { packageName, platform } = getPackageInfoByCurrentPlatform()
+    const binFilename = 'grprogress'
+    const binFileFullName = platform === 'win32' ? binFilename + '.exe' : binFilename
+    const binPath = path.resolve(__dirname, binFileFullName)
+    installUsingNPM(packageName, binFileFullName, binPath)
+  } catch (e2) {
+    // try {
+    //   await downloadDirectlyFromNPM(pkg)
+    // } catch (e3) {}
+  }
+}
+
+main()
