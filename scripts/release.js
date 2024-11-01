de99cf4a2ffa33a69657b924a7bcd03661ebc2dc
shaobeichen <shaobeichen@outlook.com>
2024-11-01 16:02:03 +0800

    feat: update release scripts

--- scripts/release.js
+++ scripts/release.js
@@ -1,21 +1,11 @@
 const path = require('path')
 const { execSync } = require('child_process')
 
-// 获取 GITHUB_TOKEN
-const githubToken = process.env.GITHUB_TOKEN
-if (!githubToken) {
-  console.error('请设置 GITHUB_TOKEN 环境变量。')
-  return
-}
-
 try {
   // npm 文件夹路径
   const npmDir = path.join(__dirname, 'npm')
 
   console.log(`正在发布 ${npmDir} 下的所有子模块...`)
-
-  // 设置 NPM_TOKEN
-  process.env.NPM_TOKEN = githubToken // 如果需要，调整此行
 
   // 执行 npm publish -r
   execSync(`npm publish -r ${npmDir}`, { stdio: 'inherit' })
